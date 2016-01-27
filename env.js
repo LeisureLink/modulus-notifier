3d95112b121966583dfc35cd0c144d9f281b4265
Zack Smith <zsmith@leisurelink.com>
2016-01-27 15:28:04 -0700

    Changed principal id to registrar

--- env.js
+++ env.js
@@ -5,5 +5,5 @@ const path = require('path');
 module.exports = {
   authenticUrl: process.env.MODULUS_NOTIFIER_AUTHENTIC_URL || 'authentic-api.leisurelink.ka/',
   keyfile: process.env.MODULUS_NOTIFIER_KEYFILE || path.resolve(__dirname, 'modulus-notifier.pem'),
-  keyId: process.env.MODULUS_NOTIFIER_KEY_ID || 'modulus-notifier/self'
+  keyId: process.env.MODULUS_NOTIFIER_KEY_ID || 'registrar/self'
 };
